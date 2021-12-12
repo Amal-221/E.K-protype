fe604916d0c82aaf0e7e66fbc57acbe4e64f6700
Amal-221 <92315480+Amal-221@users.noreply.github.com>
2021-12-12 16:58:10 +0100

    banner

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+$title ="Forside";
+$content ='<img src="Images/his2.png" class="imgLeft"/>
+<h3>Farstrup Furniture A/S </h3>
+<p>
+Farstrup Furniture A/S har siden 1910 leveret dansk håndværker. Farstrup
+Furniture A/S blev grundlagt i 1910 som et savværk. Beliggenheden tætæ på longues
+ser Skovens smukke miljø i den vestlige del af Odense er det Bedste valg, fordi 
+transporten af store træstammer dengang foregik med hestevogne. Fremstillingen af
+stole begyndte i 1918, og er derfor blevet grundlaget for udviklingen af Farstrup
+Furniture A/S den dag i dag. 
+
+</p>
+
+<p>
+I 1950erne og 1960erne kom stolen produktionen for alvor i gang, og virksomheden 
+har siden producert millioner af højkvalitets stole. Farstrup Furniture A/S produkter
+til møbelbutikker og blandt andet hospitaler i sundhedsafdelingen landet over. Med titlen
+"Unik Mennesker-Unik Stol" skaber Farstrup furniture A/S komfortable møbler til dig
+</p>';
+
+
+include 'skabeloner.php';
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ skabeloner.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+   <head>
+<mata http-equiv="Content-Type" content="text/html; charset=UFT-8">
+<title><?php echo $title; ?></title>     
+<link rel="stylesheet" type="text/css" href="styles/stylesheet.css" />
+</head>
+<body>
+    <div id="wrapper">
+       <div id="banner">
+       </div>
+
+       <nav id="navigation">
+           <ul id="nav">
+               <li><a href="index.php">Forside</a></li>
+               <li><a href="index.php">Møbler</a></li>
+               <li><a href="index.php">Shop</a></li>
+               <li><a href="index.php">Om os</a></li>
+          </ul>
+       </nav>
+<div id="content_area">
+    <?php echo $content; ?>
+</div>
+
+<div id="sidebar">
+
+</div>
+
+<footer>
+ <p>Farstrup furniture  A/S</p>
+</footer>
+            </div>
+       </body>
+</html>       
